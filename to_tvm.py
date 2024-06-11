ae7f1f1a35e1c8c7b750b76acb5eb1ed6a7841f0
Sam Purkis <sam.purkis@hotmail.co.uk>
2024-06-11 13:16:26 +0000

    blah

--- to_tvm.py
+++ to_tvm.py
@@ -4,12 +4,16 @@ import tvm.relay as relay
 import tvm
 from tvm.contrib import graph_executor
 
-onnx_model = onnx.load("/Users/user/demo_1/tts-pg/model_dec.onnx")
+onnx_model = onnx.load("/home/ubuntu/projects/oobabooga_linux/model_dec.onnx")
 
-x_in: np.ndarray = np.load("/Users/user/demo_1/tts-pg/MeloTTS/to_onnx_test/x_input.npy")
-g_in: np.ndarray = np.load("/Users/user/demo_1/tts-pg/MeloTTS/to_onnx_test/g_input.npy")
-gt_o_output: np.ndarray = np.load(
-    "/Users/user/demo_1/tts-pg/MeloTTS/to_onnx_test/o_output.npy"
+x_in = np.load(
+    "/home/ubuntu/projects/oobabooga_linux/tts/MeloTTS/to_onnx_test/x_in.npy"
+)
+g_in = np.load(
+    "/home/ubuntu/projects/oobabooga_linux/tts/MeloTTS/to_onnx_test/g_in.npy"
+)
+gt_o_output = np.load(
+    "/home/ubuntu/projects/oobabooga_linux/tts/MeloTTS/to_onnx_test/o_out.npy"
 )
 
 target = "llvm"
@@ -19,41 +23,41 @@ shape_dict = {"x.3": x_in.shape, "g": g_in.shape}
 print("Converting to Relay IR...")
 mod, params = relay.frontend.from_onnx(onnx_model, shape_dict)
 
-# print("Compiling...")
-# with tvm.transform.PassContext(opt_level=0):
-#     lib = relay.build(mod, target=target, params=params)
+print("Compiling...")
+with tvm.transform.PassContext(opt_level=0):
+    lib = relay.build(mod, target=target, params=params)
 
-# print("Loading on device...")
-# dev = tvm.device(str(target), 0)
+print("Loading on device...")
+dev = tvm.device(str(target), 0)
 
-# print("Loading module...")
-# module = graph_executor.GraphModule(lib["default"](dev))
+print("Loading module...")
+module = graph_executor.GraphModule(lib["default"](dev))
 
-# print("Setting inputs...")
-# type = "float32"
-# module.set_input("x.3", x_in)
-# module.set_input("g", g_in)
-
-# print("Running...")
-# module.run()
-
-# output_shape = gt_o_output.shape
-# tvm_output: np.ndarray = module.get_output(0, tvm.nd.empty(output_shape)).numpy()
-# print("Done!")
-# print(f"Output: {tvm_output.shape}")
-
-
-print("Compiling Executor...")
-with tvm.transform.PassContext(opt_level=1):
-    executor = relay.build_module.create_executor(
-        "graph", mod, tvm.cpu(0), target, params
-    ).evaluate()
+print("Setting inputs...")
+type = "float32"
+module.set_input("x.3", x_in)
+module.set_input("g", g_in)
 
 print("Running...")
-dtype = "float32"
-tvm_output = executor(
-    tvm.nd.array(x_in.astype(dtype)), tvm.nd.array(g_in.astype(dtype))
-).numpy()
+module.run()
 
+output_shape = gt_o_output.shape
+tvm_output: np.ndarray = module.get_output(0, tvm.nd.empty(output_shape)).numpy()
 print("Done!")
 print(f"Output: {tvm_output.shape}")
+
+
+# print("Compiling Executor...")
+# with tvm.transform.PassContext(opt_level=1):
+#     executor = relay.build_module.create_executor(
+#         "graph", mod, tvm.cpu(0), target, params
+#     ).evaluate()
+
+# print("Running...")
+# dtype = "float32"
+# tvm_output = executor(
+#     tvm.nd.array(x_in.astype(dtype)), tvm.nd.array(g_in.astype(dtype))
+# ).numpy()
+
+# print("Done!")
+# print(f"Output: {tvm_output.shape}")
---
+++ train_lightning.py
@@ -0,0 +1,375 @@
+from pathlib import Path
+import random
+import shutil
+from time import time
+
+import numpy as np
+from melo.api import TTS
+from melo.models import Generator
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+from tqdm.auto import tqdm
+import wandb
+import lightning as L
+from lightning.pytorch.loggers import WandbLogger
+
+# Speed is adjustable
+speed = 1.0
+
+# CPU is sufficient for real-time inference.
+# You can set it manually to 'cpu' or 'cuda' or 'cuda:0' or 'mps'
+device_str = "auto"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dtype = torch.float32
+
+# English
+# text = "Did you ever hear a folk tale about a giant turtle? It's a story about a turtle that carries the world on its back."
+# texts = [text]
+texts = Path("bible-web.txt").read_text().split("\n")
+texts = [t for t in texts if t != ""]
+
+# Load the model
+s = time()
+model = TTS(language="EN", device=device_str, use_onnx=False)
+print(f"Loaded model in {time() - s:.2f}s")
+speaker_ids = model.hps.data.spk2id
+output_path = "/tmp/en-default.wav"
+model.model.dec_training = []
+# model.tts_to_file("blah", speaker_ids["EN-US"], output_path, speed=speed)
+
+
+generator_params = dict(
+    initial_channel=192,  # 192
+    resblock="1",  # 1
+    resblock_kernel_sizes=[
+        5,
+        11,
+        #    3
+    ],  # [3, 7, 11]
+    resblock_dilation_sizes=[
+        [1, 3, 5],
+        [1, 3, 5],
+        # [1, 3, 5],
+    ],  # [[1, 3, 5], [1, 3, 5], [1, 3, 5]]
+    upsample_rates=[2, 4, 4, 4, 4],  # [8, 8, 2, 2, 2]
+    upsample_initial_channel=512,  # 512
+    upsample_kernel_sizes=[4, 8, 16, 4, 4],  # [16, 16, 8, 2, 2]
+    gin_channels=256,  # 256
+)
+
+distilled_generator = Generator(**generator_params).eval()
+
+total_params_dec = sum(p.numel() for p in model.model.dec.parameters())
+total_params_dec_distilled = sum(p.numel() for p in distilled_generator.parameters())
+print(f"Total params in dec: {total_params_dec}")
+print(f"Total params in dec distilled: {total_params_dec_distilled}")
+print(f"Ratio: {total_params_dec_distilled / total_params_dec:.2f}")
+
+# out_distilled = distilled_generator(
+#     torch.randn([1, 192, 299]), torch.randn([1, 256, 1])
+# )
+s = time()
+out_distilled = distilled_generator(
+    torch.randn([1, 192, 299]), torch.randn([1, 256, 1])
+)
+total_time_distilled = time() - s
+
+model.model.dec = model.model.dec.eval()
+s = time()
+out = model.model.dec(torch.randn([1, 192, 299]), torch.randn([1, 256, 1]))
+total_time = time() - s
+
+print(f"Elapsed time distilled: {total_time_distilled:.2f}s")
+print(f"Elapsed time: {total_time:.2f}s")
+print(f"Time ratio: {total_time_distilled / total_time:.2f}")
+assert out.shape == out_distilled.shape, f"{out.shape} != {out_distilled.shape}"
+del distilled_generator
+
+# use the weights of the last resblock of the original model on
+# the distilled model
+# distilled_generator.resblocks[-1].load_state_dict(
+#     model.model.dec.resblocks[-1].state_dict()
+# )
+
+# training
+
+# split the data into training and evaluation
+# np random seed
+np.random.seed(0)
+np.random.shuffle(texts)
+
+evaluation_split = 0.1
+
+training_texts = texts[: int(len(texts) * (1 - evaluation_split))]
+evaluation_texts = texts[int(len(texts) * (1 - evaluation_split) + 1) :]
+
+# training = True
+
+# lr = 1e-3
+
+# optimizer = torch.optim.Adam(distilled_generator.parameters(), lr=lr)
+# scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3_000, gamma=0.1)
+
+# epochs = 10
+# batch_size = 32
+
+# wandb.init(
+#     # set the wandb project where this run will be logged
+#     project="MeloTTS decoder-generator distillation",
+#     # track hyperparameters and run metadata
+#     config={
+#         "learning_rate": lr,
+#         "architecture": "Generator",
+#         "dataset": "web-bible",
+#         "epochs": epochs,
+#         "batch_size": batch_size,
+#         "evaluation_split": evaluation_split,
+#         "speed": speed,
+#         "model_hyperparameters": generator_params,
+#         "model_size_ratio": total_params_dec_distilled / total_params_dec,
+#         "model_time_ratio": total_time_distilled / total_time,
+#     },
+# )
+
+# for epoch in range(epochs):
+#     print(f"Epoch {epoch + 1}/{epochs}")
+
+#     epoch_loss = 0
+#     epoch_evaluation_loss = 0
+
+#     steps = len(texts) // batch_size
+
+#     for step in tqdm(range(steps), desc="Steps"):
+#         training = step % 10 != 0
+#         print(f"Epoch {epoch + 1}/{epochs}, Step {step + 1}/{steps}")
+
+#         batch = []
+
+#         model.model.dec_training = []
+
+#         # generate the batch of data
+#         for i in tqdm(
+#             range(batch_size),
+#             desc=f"Batch generate {'training' if training else 'evaluation'}",
+#             leave=False,
+#         ):
+#             if training:
+#                 text = training_texts[step * batch_size + i]
+#             else:
+#                 text = evaluation_texts
+
+#             model.model.dec_training.append({"text": text})
+#             model.tts_to_file(
+#                 text, speaker_ids["EN-Default"], output_path, speed=speed, quiet=True
+#             )
+
+#             # load batch of data to train on
+#             point = model.model.dec_training.pop(0)
+#             x_in = np.load(point["x_in_path"])
+#             x_in = torch.tensor(x_in, dtype=torch.float32).to(device)
+
+#             g_in = np.load(point["g_in_path"])
+#             g_in = torch.tensor(g_in, dtype=torch.float32).to(device)
+
+#             o_out = np.load(point["o_out_path"])
+#             o_out = torch.tensor(o_out, dtype=torch.float32).to(device)
+
+#             batch.append((x_in, g_in, o_out))
+
+#         # clear the numpy files
+#         for point in model.model.dec_training:
+#             Path(point["x_in_path"]).unlink(missing_ok=True)
+#             Path(point["g_in_path"]).unlink(missing_ok=True)
+#             Path(point["o_out_path"]).unlink(missing_ok=True)
+#         shutil.rmtree("dec_distill/data", ignore_errors=True)
+#         Path("dec_distill/data").mkdir(exist_ok=True)
+
+#         # train on batch
+#         if training:
+#             distilled_generator.train()
+#         else:
+#             distilled_generator.eval()
+
+#         # run the whole batch through then run backwards and optimize
+#         batch_loss = 0
+#         if training:
+#             optimizer.zero_grad()  # zero the gradient buffers
+
+#         for x_in, g_in, o_out in tqdm(
+#             batch, desc=f"Batch {'training' if training else 'evaluation'}", leave=False
+#         ):
+#             output = distilled_generator(x_in, g_in)  # forward pass
+#             loss = F.mse_loss(output, o_out)
+#             loss = loss * torch.sqrt(
+#                 torch.tensor(len(output)).float()
+#             )  # weight the loss
+#             if training:
+#                 loss.backward()  # backpropagation
+#             batch_loss += loss.item()
+#         batch_loss /= len(batch)
+
+#         if training:
+#             scheduler.step()
+
+#         print(
+#             f"{'Training' if training else 'Evaluation'} Batch Loss: {batch_loss / len(batch)}"
+#         )
+
+#         # wandb log epoch, step, evaluation/training losses
+#         if training:
+#             wandb.log(
+#                 {
+#                     "step": step,
+#                     "loss": batch_loss,
+#                 }
+#             )
+#         else:
+#             wandb.log(
+#                 {
+#                     "step": step,
+#                     "evaluation_loss": batch_loss,
+#                 }
+#             )
+#     # wandb log epoch, step, evaluation/training losses
+#     if training:
+#         wandb.log(
+#             {
+#                 "step": step,
+#                 "loss": batch_loss,
+#             }
+#         )
+#     else:
+#         wandb.log(
+#             {
+#                 "step": step,
+#                 "evaluation_loss": batch_loss,
+#             }
+#         )
+
+#     Path("dec_distill/checkpoints").mkdir(exist_ok=True)
+
+#     # save model
+#     print("Saving model...")
+#     torch.save(
+#         distilled_generator.state_dict(),
+#         f"dec_distill/checkpoints/model_{epoch}_loss_{batch_loss}.pt",
+#     )
+
+
+class GeneratorDataset(Dataset):
+    def __init__(self, texts, model: TTS, speaker_ids, speed=1.0, batch_size=1):
+        self.texts = texts
+        self.model = model
+        self.speaker_ids = speaker_ids
+        self.speed = speed
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        data = []
+        for i in range(self.batch_size):
+            text = self.texts[idx + i]
+
+            self.model.model.dec_training = []
+            self.model.model.dec_training.append({"text": text})
+            self.model.tts_to_file(
+                text,
+                self.speaker_ids["EN-Default"],
+                output_path,
+                speed=self.speed,
+                quiet=True,
+            )
+
+            # load batch of data to train on
+            point = self.model.model.dec_training.pop(0)
+            # x_in = point["x_in"]
+            # g_in = point["g_in"]
+            # o_out = point["o_out"]
+            data.append((point["x_in"], point["g_in"], point["o_out"]))
+
+        return data
+
+
+# define the LightningModule
+class LitGeneratorWrapper(L.LightningModule):
+    def __init__(
+        self,
+        generator_params,
+        learning_rate: float,
+    ):
+        super().__init__()
+        self.generator_params = generator_params
+        self.learning_rate = learning_rate
+
+        self.save_hyperparameters()
+
+        self.distilled_generator = Generator(**generator_params)
+
+    def training_step(self, batch, batch_idx):
+        batch_loss = 0
+        for x_in, g_in, o_out in batch:
+            x_in = x_in.squeeze(0)
+            g_in = g_in.squeeze(0)
+            o_out = o_out.squeeze(0)
+            output = self.distilled_generator(x_in, g_in)
+            loss = F.mse_loss(output, o_out)
+            batch_loss += loss.item()
+        self.log("train/loss", batch_loss)
+        return batch_loss
+
+    def validation_step(self, batch, batch_idx):
+        batch_loss = 0
+        for x_in, g_in, o_out in batch:
+            x_in = x_in.squeeze(0)
+            g_in = g_in.squeeze(0)
+            o_out = o_out.squeeze(0)
+            output = self.distilled_generator(x_in, g_in)
+            loss = F.mse_loss(output, o_out)
+            batch_loss += loss.item()
+        self.log("val/loss", batch_loss)
+        return batch_loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(
+            self.distilled_generator.parameters(), lr=self.learning_rate
+        )
+
+
+batch_size = 2
+lr = 1e-1
+
+# train and eval dataset and data loaders
+train_dataset = GeneratorDataset(
+    texts, model, speaker_ids, speed=speed, batch_size=batch_size
+)
+train_data_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+
+eval_dataset = GeneratorDataset(
+    evaluation_texts, model, speaker_ids, speed=speed, batch_size=batch_size
+)
+eval_data_loader = DataLoader(eval_dataset, batch_size=1, shuffle=False)
+
+model_wrapper = LitGeneratorWrapper(
+    generator_params=generator_params,
+    learning_rate=lr,
+)
+# wandb_logger = WandbLogger(project="MeloTTS decoder-generator distillation")
+
+
+trainer = L.Trainer(
+    limit_train_batches=100,
+    max_epochs=1,
+    precision=16,
+    fast_dev_run=False,
+    enable_progress_bar=True,
+    # logger=wandb_logger
+)
+trainer.fit(
+    model=model_wrapper,
+    train_dataloaders=train_data_loader,
+    val_dataloaders=eval_data_loader,
+)
