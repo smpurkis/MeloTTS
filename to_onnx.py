370a36784f51a30b1531253b54925f8f56786c25
Sam Purkis <sam.purkis@hotmail.co.uk>
2024-06-14 15:57:20 +0000

    make onnx model easier to use

--- to_onnx.py
+++ to_onnx.py
@@ -19,9 +19,7 @@ device = "cpu"  # Will automatically use GPU if available
 text = "It's a story that's been told in many different languages."
 
 # Load the model
-s = time()
 model = TTS(language="EN", device=device, use_onnx=False)
-print(f"Loaded model in {time() - s:.2f}s")
 speaker_ids = model.hps.data.spk2id
 
 model_model_dec = torch.jit.script(
@@ -43,19 +41,19 @@ s = time()
 model.tts_to_file(text, speaker_ids["EN-US"], output_path, speed=speed)
 print(f"Elapsed time: {time() - s:.2f}s")
 
-# # s = time()
-# # model.tts_to_file(text, speaker_ids["EN-US"], output_path, speed=speed)
-# # print(f"Elapsed time: {time() - s:.2f}s")
+s = time()
+model.tts_to_file(text, speaker_ids["EN-US"], output_path, speed=speed)
+print(f"Elapsed time: {time() - s:.2f}s")
 
 
-# # model = TTS(language="EN", device=device, use_onnx=True)
-# # s = time()
-# # model.tts_to_file(text, speaker_ids["EN-US"], output_path, speed=speed)
-# # print(f"Elapsed time: {time() - s:.2f}s")
+model = TTS(language="EN", device=device, use_onnx=True)
+s = time()
+model.tts_to_file(text, speaker_ids["EN-US"], output_path, speed=speed)
+print(f"Elapsed time: {time() - s:.2f}s")
 
-# # s = time()
-# # model.tts_to_file(text, speaker_ids["EN-US"], output_path, speed=speed)
-# # print(f"Elapsed time: {time() - s:.2f}s")
+s = time()
+model.tts_to_file(text, speaker_ids["EN-US"], output_path, speed=speed)
+print(f"Elapsed time: {time() - s:.2f}s")
 
 # print(f"Type before script: {type(model.model.dec)}")
 # model.model.dec = torch.jit.script(
@@ -71,27 +69,21 @@ print(f"Elapsed time: {time() - s:.2f}s")
 # Path(output_path).unlink(missing_ok=True)
 
 # ground truth for dec
-x_in = np.load(
-    "/home/ubuntu/projects/oobabooga_linux/tts/MeloTTS/to_onnx_test/x_in.npy"
-)
-g_in = np.load(
-    "/home/ubuntu/projects/oobabooga_linux/tts/MeloTTS/to_onnx_test/g_in.npy"
-)
-gt_o_output = np.load(
-    "/home/ubuntu/projects/oobabooga_linux/tts/MeloTTS/to_onnx_test/o_out.npy"
-)
+# x_in = np.random.random([1, 192, 299]).astype(np.float32)
+# g_in = np.random.random([1, 256, 1]).astype(np.float32)
+# gt_o_output = model.model.dec(torch.tensor(x_in), torch.tensor(g_in)).detach().numpy()
 
 
-with torch.no_grad():
-    model_model_dec.eval()
-    s = time()
-    torch_o_output = (
-        model_model_dec(torch.tensor(x_in), torch.tensor(g_in)).detach().numpy()
-    )
-    print(f"Elapsed time: {time() - s:.2f}s")
-    assert np.allclose(torch_o_output, gt_o_output, atol=1e-4)
+# with torch.no_grad():
+#     model_model_dec.eval()
+#     s = time()
+#     torch_o_output = (
+#         model_model_dec(torch.tensor(x_in), torch.tensor(g_in)).detach().numpy()
+#     )
+#     print(f"Elapsed time: {time() - s:.2f}s")
+#     assert np.allclose(torch_o_output, gt_o_output, atol=1e-4)
 
-    print("Scripted fp32 Torch model works correctly!")
+#     print("Scripted fp32 Torch model works correctly!")
 
 #     quantized_torch_model = torch.quantization.quantize_dynamic(
 #         model_model_dec,
@@ -157,46 +149,48 @@ with torch.no_grad():
 
 
 # Load the ONNX model
-import onnx
-import onnxruntime as ort
+# import onnx
+# import onnxruntime as ort
 
-# from onnxruntime.quantization import quantize_dynamic, QuantType, quantize_static
-# from onnxruntime.quantization.preprocess import quant_pre_process
+# # from onnxruntime.quantization import quantize_dynamic, QuantType, quantize_static
+# # from onnxruntime.quantization.preprocess import quant_pre_process
 
-# from onnxruntime.tools.optimize_onnx_model import optimize_model
+# # from onnxruntime.tools.optimize_onnx_model import optimize_model
 
 
-# # export model.model.dec to ONNX
-model_fp32_path = "model_dec.onnx"
+# # # export model.model.dec to ONNX
+# model_fp32_path = "model_dec.onnx"
 # torch.onnx.export(
-#     model_model_dec,
+#     model.model.dec,
 #     (torch.zeros([1, 192, 299]), torch.zeros([1, 256, 1])),
 #     model_fp32_path,
 #     input_names=["x", "g"],
 #     output_names=["x"],
 #     dynamic_axes={"x": {0: "batch", 2: "length"}, "g": {0: "batch", 2: "length"}},
-#     opset_version=18,
+#     opset_version=17,
 # )
 
 
-# torch.onnx.dynamo_export(
-#     model_model_dec,
-#     x=torch.zeros([1, 192, 299]),
-#     g=torch.zeros([1, 256, 1]),
-#     # export_options=torch.onnx.ExportOptions(dynamic_shapes=True),
-# ).save(model_fp32_path)
+# # torch.onnx.dynamo_export(
+# #     model_model_dec,
+# #     x=torch.zeros([1, 192, 299]),
+# #     g=torch.zeros([1, 256, 1]),
+# #     # export_options=torch.onnx.ExportOptions(dynamic_shapes=True),
+# # ).save(model_fp32_path)
 
 
 # onnx_model = onnx.load(model_fp32_path)
-# onnx.checker.check_model(onnx_model, full_check=True)
-# # print(onnx_model.graph.input)
+# # # onnx.checker.check_model(onnx_model, full_check=True)
+# # # print(onnx_model.graph.input)
 
-ort_session = ort.InferenceSession(model_fp32_path)
+# ort_session = ort.InferenceSession(model_fp32_path)
 
-s = time()
-onnx_o_output = ort_session.run(None, {"x.3": x_in, "g": g_in})[0]
-print(f"fp32 Elapsed time: {time() - s:.2f}s")
-assert np.allclose(onnx_o_output, gt_o_output, atol=1e-4)
+# s = time()
+# onnx_o_output = ort_session.run(
+#     None, {onnx_model.graph.input[0].name: x_in, onnx_model.graph.input[1].name: g_in}
+# )[0]
+# print(f"fp32 Elapsed time: {time() - s:.2f}s")
+# assert np.allclose(onnx_o_output, gt_o_output, atol=1e-4)
 
 # print("ONNX model works correctly!")
 
@@ -318,72 +312,72 @@ assert np.allclose(onnx_o_output, gt_o_output, atol=1e-4)
 #     outputs_channel_order=ChannelOrder.PYTORCH,
 # )
 
-# keras_model.save("model_dec.keras")
+# # keras_model.save("model_dec.keras")
 
-import keras
+# import keras
 
-keras_model = keras.models.load_model("model_dec.keras")
+# # keras_model = keras.models.load_model("model_dec.keras")
 
-s = time()
-out = keras_model.predict({"input_1": x_in, "input_2": g_in})
-print(f"Elapsed time: {time() - s:.2f}s")
-assert np.allclose(out, gt_o_output, atol=1e-4)
+# s = time()
+# out = keras_model.predict({"input_1": x_in, "input_2": g_in})
+# print(f"Elapsed time: {time() - s:.2f}s")
+# assert np.allclose(out, gt_o_output, atol=1e-4)
 
-s = time()
-out = keras_model.predict({"input_1": x_in, "input_2": g_in})
-print(f"Elapsed time: {time() - s:.2f}s")
-assert np.allclose(out, gt_o_output, atol=1e-4)
+# s = time()
+# out = keras_model.predict({"input_1": x_in, "input_2": g_in})
+# print(f"Elapsed time: {time() - s:.2f}s")
+# assert np.allclose(out, gt_o_output, atol=1e-4)
 
-import tensorflow as tf
-
-
-# def representative_dataset():
-#     for _ in range(10):
-#         yield [
-#             np.random.rand(1, 192, np.random.randint(200, 600)).astype(np.float32),
-#             np.random.rand(1, 256, 1).astype(np.float32),
-#         ]
+# import tensorflow as tf
 
 
-converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-# converter.representative_dataset = representative_dataset
-converter.target_spec.supported_ops = [
-    tf.lite.OpsSet.TFLITE_BUILTINS,
-    # tf.lite.OpsSet.TFLITE_BUILTINS_INT8,
-    # tf.lite.OpsSet.EXPERIMENTAL_TFLITE_BUILTINS_ACTIVATIONS_INT16_WEIGHTS_INT8,
-]
-# converter.inference_input_type = tf.int8
-# converter.inference_output_type = tf.int8
+# # def representative_dataset():
+# #     for _ in range(10):
+# #         yield [
+# #             np.random.rand(1, 192, np.random.randint(200, 600)).astype(np.float32),
+# #             np.random.rand(1, 256, 1).astype(np.float32),
+# #         ]
 
-tflite_model = converter.convert()
-with open("model_dec.tflite", "wb") as f:
-    f.write(tflite_model)
 
-tflite_model = Path("model_dec.tflite").read_bytes()
+# converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
+# converter.optimizations = [tf.lite.Optimize.DEFAULT]
+# # converter.representative_dataset = representative_dataset
+# converter.target_spec.supported_ops = [
+#     tf.lite.OpsSet.TFLITE_BUILTINS,
+#     # tf.lite.OpsSet.TFLITE_BUILTINS_INT8,
+#     # tf.lite.OpsSet.EXPERIMENTAL_TFLITE_BUILTINS_ACTIVATIONS_INT16_WEIGHTS_INT8,
+# ]
+# # converter.inference_input_type = tf.int8
+# # converter.inference_output_type = tf.int8
 
-interpreter = tf.lite.Interpreter(model_content=tflite_model, num_threads=4)
-interpreter.allocate_tensors()
+# tflite_model = converter.convert()
+# with open("model_dec.tflite", "wb") as f:
+#     f.write(tflite_model)
 
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+# tflite_model = Path("model_dec.tflite").read_bytes()
 
-interpreter.resize_tensor_input(input_details[0]["index"], x_in.shape)
-interpreter.resize_tensor_input(input_details[1]["index"], g_in.shape)
+# interpreter = tf.lite.Interpreter(model_content=tflite_model, num_threads=4)
+# interpreter.allocate_tensors()
 
-interpreter.allocate_tensors()
+# input_details = interpreter.get_input_details()
+# output_details = interpreter.get_output_details()
 
-interpreter.set_tensor(input_details[0]["index"], x_in)
-interpreter.set_tensor(input_details[1]["index"], g_in)
+# interpreter.resize_tensor_input(input_details[0]["index"], x_in.shape)
+# interpreter.resize_tensor_input(input_details[1]["index"], g_in.shape)
 
-s = time()
-interpreter.invoke()
-tflite_o_output = interpreter.get_tensor(output_details[0]["index"])
-print(f"Elapsed time: {time() - s:.2f}s")
-assert np.allclose(tflite_o_output, gt_o_output, atol=1e-4)
+# interpreter.allocate_tensors()
 
-s = time()
-interpreter.invoke()
-tflite_o_output = interpreter.get_tensor(output_details[0]["index"])
-print(f"Elapsed time: {time() - s:.2f}s")
-assert np.allclose(tflite_o_output, gt_o_output, atol=1e-4)
+# interpreter.set_tensor(input_details[0]["index"], x_in)
+# interpreter.set_tensor(input_details[1]["index"], g_in)
+
+# s = time()
+# interpreter.invoke()
+# tflite_o_output = interpreter.get_tensor(output_details[0]["index"])
+# print(f"Elapsed time: {time() - s:.2f}s")
+# assert np.allclose(tflite_o_output, gt_o_output, atol=1e-4)
+
+# s = time()
+# interpreter.invoke()
+# tflite_o_output = interpreter.get_tensor(output_details[0]["index"])
+# print(f"Elapsed time: {time() - s:.2f}s")
+# assert np.allclose(tflite_o_output, gt_o_output, atol=1e-4)
