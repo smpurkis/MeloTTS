eb0584365d052ab379215cfbc691a89669abf6b6
Sam Purkis <sam.purkis@hotmail.co.uk>
2024-06-24 15:04:32 +0100

    Add wandb for external loss tracking and fix paths

--- train.py
+++ train.py
@@ -10,8 +10,9 @@ import torch
 import torch.nn.functional as F
 from tqdm.auto import tqdm
 import wandb
+import re
 
-# Speed is adjustable
+# # Speed is adjustable
 speed = 1.0
 
 # CPU is sufficient for real-time inference.
@@ -19,21 +20,87 @@ speed = 1.0
 device_str = "gpu" if torch.cuda.is_available() else "cpu"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dtype = torch.float32
+# # CPU is sufficient for real-time inference.
+# # You can set it manually to 'cpu' or 'cuda' or 'cuda:0' or 'mps'
+device = "cpu"  # Will automatically use GPU if available
 
-# English
-# text = "Did you ever hear a folk tale about a giant turtle? It's a story about a turtle that carries the world on its back."
-# texts = [text]
-texts = Path("bible-web.txt").read_text().split("\n")
+# # English
+# # text = "Did you ever hear a folk tale about a giant turtle? It's a story about a turtle that carries the world on its back."
+# # texts = [text]
 
-# Load the model
+
+def parse_text(text):
+    # remove ', ", ”, “
+    text = text.replace("'", "")
+    text = text.replace('"', "")
+    text = text.replace("”", "")
+    text = text.replace("“", "")
+
+    # remove all \n
+    text = text.replace("\n", "")
+
+    # split by ., !, ?, ;, ',' using regex
+    text = re.split(
+        r"(?<!\d)\.(?!\d)|(?<!\d)!(?!\d)|(?<!\d)\?(?!\d)|(?<!\d);(?!\d)",
+        text,
+    )
+
+    texts = [t for t in text if t.strip() != ""]
+
+    # join the texts to make sure the length of the text is less than target_word_length
+    target_word_length = 15
+    min_word_length = 6
+    max_word_length = 30
+    joined_texts = []
+    joined_text = ""
+    lens = {}
+    for t in texts:
+        combined_length = len(joined_text.split()) + len(t.split())
+        if (
+            combined_length <= target_word_length
+            or len(t.split()) < min_word_length
+            and combined_length <= max_word_length
+        ):
+            joined_text += " " + t
+        else:
+            joined_length = len(joined_text.split())
+
+            if min_word_length <= joined_length <= max_word_length:
+
+                # ensure all commas are followed by a space
+                joined_text = re.sub(r",(?=[^\s])", ", ", joined_text)
+
+                joined_texts.append(joined_text.strip())
+
+            if joined_length in lens:
+                lens[joined_length] += 1
+            else:
+                lens[joined_length] = 1
+
+            joined_text = t
+
+    lens = sorted(lens.items(), key=lambda x: x[0])
+
+    shortest_s = min(joined_texts, key=lambda x: len(x))
+    longest_s = max(joined_texts, key=lambda x: len(x))
+    return joined_texts
+
+
+# texts = parse_text(Path("bible-web.txt").read_text())
+
+# # Load the model
 s = time()
 model = TTS(language="EN", device=device_str, use_onnx=False)
 print(f"Loaded model in {time() - s:.2f}s")
 speaker_ids = model.hps.data.spk2id
 output_path = "/tmp/en-default.wav"
 model.model.dec_training = []
-# model.tts_to_file("blah", speaker_ids["EN-US"], output_path, speed=speed)
-
+model.tts_to_file(
+    "Now those who sealed were: Nehemiah the governor, the son of Hacaliah, and Zedekiah, Seraiah, Azariah, Jeremiah, Pashhur, Amariah, Malchijah, Hattush, Shebaniah, Malluch, Harim, Meremoth, Obadiah, Daniel, Ginnethon, Baruch, Meshullam, Abijah, Mijamin, Maaziah, Bilgai, and Shemaiah",
+    speaker_ids["EN-Default"],
+    "en-default.wav",
+    speed=speed,
+)
 
 generator_params = dict(
     initial_channel=192,  # 192
@@ -80,166 +147,3 @@ print(f"Elapsed time distilled: {total_time_distilled:.2f}s")
 print(f"Elapsed time: {total_time:.2f}s")
 print(f"Time ratio: {total_time_distilled / total_time:.2f}")
 assert out.shape == out_distilled.shape, f"{out.shape} != {out_distilled.shape}"
-
-# use the weights of the last resblock of the original model on
-# the distilled model
-# distilled_generator.resblocks[-1].load_state_dict(
-#     model.model.dec.resblocks[-1].state_dict()
-# )
-
-# training
-
-# split the data into training and evaluation
-# np random seed
-np.random.seed(0)
-np.random.shuffle(texts)
-
-evaluation_split = 0.01
-
-training_texts = texts[: int(len(texts) * (1 - evaluation_split))]
-evaluation_texts = texts[int(len(texts) * (1 - evaluation_split) + 1) :]
-
-training = True
-
-lr = 1e-3
-
-epochs = 10
-batch_size = 128
-steps = len(training_texts) // batch_size
-eval_steps = len(evaluation_texts) // batch_size
-
-distilled_generator.train().to(device)
-optimizer = torch.optim.Adam(distilled_generator.parameters(), lr=lr)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2 * steps, gamma=0.1)
-
-wandb.init(
-    # set the wandb project where this run will be logged
-    project="MeloTTS decoder-generator distillation",
-    # track hyperparameters and run metadata
-    config={
-        "learning_rate": lr,
-        "epochs": epochs,
-        "batch_size": batch_size,
-        "evaluation_split": evaluation_split,
-        "speed": speed,
-        "model_hyperparameters": generator_params,
-        "model_size_ratio": total_params_dec_distilled / total_params_dec,
-        "model_time_ratio": total_time_distilled / total_time,
-    },
-)
-
-for epoch in range(epochs):
-    print(f"Epoch {epoch + 1}/{epochs}")
-
-    epoch_loss = 0
-    epoch_evaluation_loss = 0
-
-    eval_step_counter = 0
-
-    for step in tqdm(range(steps), desc="Steps"):
-        training = step % 10 != 0
-        print(f"Epoch {epoch + 1}/{epochs}, Step {step + 1}/{steps}")
-
-        batch = []
-
-        model.model.dec_training = []
-
-        # generate the batch of data
-        for i in tqdm(
-            range(batch_size),
-            desc=f"Batch generate {'training' if training else 'evaluation'}",
-            leave=False,
-        ):
-            if training:
-                text = training_texts[step * batch_size + i]
-            else:
-                if eval_steps == eval_step_counter:
-                    eval_step_counter = 0
-                text = evaluation_texts[eval_step_counter * batch_size + i]
-                eval_step_counter += 1
-
-            model.model.dec_training.append({"text": text})
-            model.tts_to_file(
-                text, speaker_ids["EN-Default"], output_path, speed=speed, quiet=True
-            )
-
-            # load batch of data to train on
-            point = model.model.dec_training.pop(0)
-            x_in = point["x_in"].type(dtype).to(device)
-            g_in = point["g_in"].type(dtype).to(device)
-            o_out = point["o_out"].type(dtype).to(device)
-            batch.append((x_in, g_in, o_out))
-
-        # train on batch
-        if training:
-            distilled_generator.train()
-        else:
-            distilled_generator.eval()
-
-        # run the whole batch through then run backwards and optimize
-        batch_loss = 0
-        if training:
-            optimizer.zero_grad()  # zero the gradient buffers
-
-        for x_in, g_in, o_out in tqdm(
-            batch, desc=f"Batch {'training' if training else 'evaluation'}", leave=False
-        ):
-            output = distilled_generator(x_in, g_in)  # forward pass
-            loss = F.mse_loss(output, o_out)
-            if training:
-                loss.backward()  # backpropagation
-            batch_loss += loss.item()
-        batch_loss /= len(batch)
-        if training:
-            epoch_loss += batch_loss
-        else:
-            epoch_evaluation_loss += batch_loss
-
-        if training:
-            optimizer.step()  # Does the update
-
-        print(
-            f"{'Training' if training else 'Evaluation'} Batch Loss: {batch_loss / len(batch)}"
-        )
-
-        # wandb log epoch, step, evaluation/training losses
-        if training:
-            wandb.log(
-                {
-                    "step": epoch * steps + step,
-                    "loss": epoch_loss,
-                }
-            )
-        else:
-            wandb.log(
-                {
-                    "step": epoch * steps + step,
-                    "evaluation_loss": epoch_evaluation_loss,
-                }
-            )
-    # wandb log epoch, step, evaluation/training losses
-    if training:
-        wandb.log(
-            {
-                "epoch": epoch,
-                "loss": batch_loss,
-            }
-        )
-    else:
-        wandb.log(
-            {
-                "epoch": epoch,
-                "evaluation_loss": batch_loss,
-            }
-        )
-
-    scheduler.step()
-
-    Path("dec_distill/checkpoints").mkdir(exist_ok=True)
-
-    # save model
-    print("Saving model...")
-    torch.save(
-        distilled_generator.state_dict(),
-        f"dec_distill/checkpoints/model_{epoch}_loss_{batch_loss}.pt",
-    )
